eb1ce41210310510ae337c59ed50ccf394fa5af6
John Doneth <doneth7@gmail.com>
2021-01-13 00:24:28 -0500

    WIP 5 solution

---
+++ 5/app/Main.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import Flow ((|>))
+import Lib
+import System.Exit
+import System.IO
+import Text.Parsec
+
+main :: IO ()
+main = do
+  input <- readFile "input.txt"
+  return ()
